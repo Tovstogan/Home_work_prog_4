d5e8af58dd45603ac1ab00532cb17af4e62b9429
Tovstogan <andrey-tovstogan@yandex.ru>
2022-07-10 21:18:21 +0400

    задача_25

---
+++ Задача_25/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+Console.Clear();
+Console.WriteLine("Задача 25");
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+
+int A = GetNumber("Введите число А");
+int B = GetNumber("Введите число В");
+
+double result =(Math.Pow(A,B));
+
+Console.WriteLine(result);
